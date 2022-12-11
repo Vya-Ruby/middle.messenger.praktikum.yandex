0274e89f05dbae5bf37558c4a1abb39ea233bc60
Vya-Ruby <mr.craftman88@gmail.com>
2022-12-12 00:25:02 +0300

    Completion of edits on the list.

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const PORT = 3000;
 
 app.use(express.static('./dist'));
 
-app.listen(PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log(`Example app listening on port ${PORT}! \n server: http://localhost:${PORT}`);
 });
